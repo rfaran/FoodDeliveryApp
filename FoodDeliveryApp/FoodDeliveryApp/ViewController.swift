4202d1a98e4faf20dcc56143906130b744e7c190
faranrasheed <faran_rasheed@hotmail.com>
2020-10-28 16:55:42 +0500

    Init Project

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Faran Rasheed on 28/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
